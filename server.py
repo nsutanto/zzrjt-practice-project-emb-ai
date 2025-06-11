16a0fac43a59e17c23e81073d147889c91bfcbd7
nsutanto <nicholas.sutanto@gmail.com>
2025-06-11 14:28:08 -0500

    Update solution

--- server.py
+++ server.py
@@ -24,12 +24,15 @@ def sent_analyzer():
     label = response['label']
     score = response['score']
 
-    # Return a formatted string with the sentiment label and score
-    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
-
+    # Check if the label is None, indicating an error or invalid input
+    if label is None:
+        return "Invalid input! Try again."
+    else:
+        # Return a formatted string with the sentiment label and score
+        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
